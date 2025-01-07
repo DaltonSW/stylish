725d74768b2d5f86b85f9b822054d86231e61195
Dalton Williams <daltonstwilliams@gmail.com>
2025-01-07 10:45:02 -0600

    Implement dircolors check and AltScreen use

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"log"
+	"os/exec"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"go.dalton.dog/colorgen/tui"
 )
 
 func main() {
-	w, h := tui.GetTermSize()
-	program := tea.NewProgram(tui.NewLandingModel(w, h), tea.WithAltScreen())
+	// Check that dircolors command is installed
+	_, err := exec.LookPath("dircolors")
+	if err != nil {
+		log.Fatal("Package `dircolors` not found on PATH. Please install it before continuing.")
+	}
+
+	// Clear screen -- Not needed if tea.WithAltScreen() is used
+	// cmd := exec.Command("clear")
+	// cmd.Stdout = os.Stdout
+	// cmd.Run()
+
+	program := tea.NewProgram(tui.NewLandingModel(), tea.WithAltScreen())
 	if _, err := program.Run(); err != nil {
 		log.Fatalf("Error starting program: %v\n", err)
 	}
