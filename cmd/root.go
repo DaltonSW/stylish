b10eeee812165b8ccb9d74e029bdb823995fb496
Dalton Williams <daltonstwilliams@gmail.com>
2025-01-11 16:19:43 -0600

    Swapped command handling to spf13/cobra

---
+++ cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"go.dalton.dog/stylish/internal/tui"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/log"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "stylish",
+	Short: "stylish is a simple and intuitive path to a prettier ls experience",
+	Run: func(cmd *cobra.Command, args []string) {
+		program := tea.NewProgram(tui.NewLandingModel(), tea.WithAltScreen())
+		if _, err := program.Run(); err != nil {
+			log.Fatalf("Error running program: %v\n", err)
+		}
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"os"
 	"os/exec"
-	"path/filepath"
-	"strings"
-	"syscall"
 
-	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/log"
-	"go.dalton.dog/stylish/tui"
+	"go.dalton.dog/stylish/cmd"
 )
-
-// TODO: Swap to cobra for commands
-// TODO: Move stuff into `cmd` and `internal` to publish on pkg.go.dev
 
 // TODO: Look into `goreleaser`
 
@@ -24,82 +14,19 @@ import (
 // TODO: Clean up and comment code
 
 func main() {
-	// Check that dircolors command is installed
-	_, err := exec.LookPath("dircolors")
-	if err != nil {
-		log.Fatal("Package `dircolors` not found on PATH. Please install it before continuing.")
-	}
+	// Check that `dircolors` is installed
+	checkCommand("dircolors")
 
-	_, err = exec.LookPath("ls")
-	if err != nil {
-		log.Fatal("Package `ls` not found on PATH. Please install it before continuing.")
-	}
+	// Check that `ls` is installed
+	checkCommand("ls")
 
-	// If no arguments are provided, just run the TUI
-	if len(os.Args) == 1 {
-		program := tea.NewProgram(tui.NewLandingModel(), tea.WithAltScreen())
-		if _, err := program.Run(); err != nil {
-			log.Fatalf("Error running program: %v\n", err)
-		}
-		return
-	} else {
-		handleArgs()
-	}
+	// If dependencies are fulfilled, kick it off to Cobra
+	cmd.Execute()
 }
 
-func handleArgs() {
-	// Else, evaluate what the command is asking for
-	switch os.Args[1] {
-	case "help":
-		log.Printf("Help command")
-
-	case "preview":
-		output := doApply()
-		output = strings.TrimPrefix(output, "LS_COLORS='")
-		output = strings.TrimSpace(output)
-		output = strings.TrimSuffix(output, "';\nexport LS_COLORS")
-
-		os.Setenv("LS_COLORS", output)
-
-		binary, pathErr := exec.LookPath("ls")
-		if pathErr != nil {
-			log.Fatal(errors.New("Path err: " + pathErr.Error()))
-		}
-
-		execErr := syscall.Exec(binary, []string{"ls", "--color=auto"}, os.Environ())
-		if execErr != nil {
-			log.Fatal(errors.New("Exec err: " + execErr.Error()))
-		}
-
-	case "apply":
-		fmt.Print(doApply())
-	case "apply-8bit":
-		tui.EightBitMode = true
-		fmt.Print(doApply())
-
-	default:
-		log.Fatal("Command not found")
-	}
-
-}
-
-func doApply() string {
-	var theme string
-	if len(os.Args) < 3 {
-		theme = "default"
-	} else {
-		theme = os.Args[2]
-	}
-
-	err := tui.GetTheme(theme).GenerateDirColors()
+func checkCommand(command string) {
+	_, err := exec.LookPath(command)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Package " + command + " not found on PATH. Please install it before continuing.")
 	}
-
-	cmd := exec.Command("dircolors", filepath.Join(tui.ThemeConfigFolder, theme, ".dircolors"))
-	cmdOut, cmdErr := cmd.Output()
-	if cmdErr != nil {
-		log.Fatal(cmdErr.Error() + string(cmdOut))
-	}
-	return string(cmdOut)
 }
